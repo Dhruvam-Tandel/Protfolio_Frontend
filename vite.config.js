12f9146301a733e08970ca486de0e5b0f009ba76
Dhruvam-Tandel <dhruvamtandel12@gmail.com>
2026-02-25 23:41:59 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  bash:"Protfolio_Frontend",
+  bash:"/Protfolio_Frontend/",
   plugins: [react(),
     tailwindcss(),],
     server: {
